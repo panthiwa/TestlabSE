a30541eec85d64d08b4f05b0bbf862f0c7ef30a3
panthiwa <thiwab59@gmail.com>
2019-01-09 14:13:35 +0700

    entity food.java - close #5

---
+++ food.java
@@ -0,0 +1,7 @@
+public class Register {
+    private @NonNull Long maidId;
+    private @NonNull String maidName;
+    private @NonNull String maidAddress;
+    private @NonNull String maidEmail;
+    private @NonNull String maidPhone;
+}
